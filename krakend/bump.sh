7f8bb04e08a55c94299ee8c0cffcaba55752a750
Naaman Curtis <ncurtis@kidsloop.live>
2022-04-07 16:09:39 +0100

    feat(krakend): migrate to enterprise

--- krakend/bump.sh
+++ krakend/bump.sh
@@ -1,12 +1,12 @@
-VERSION='0.0.14'
-ECR_URL='246958245973.dkr.ecr.eu-west-2.amazonaws.com/krakend'
+VERSION='0.0.17'
+ECR_URL='246958245973.dkr.ecr.eu-west-2.amazonaws.com/krakend-enterprise'
 VERSION="$(echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')"
 sed -i -e "1s/.*/VERSION='$VERSION'/" bump.sh
 echo "Next Version: $VERSION"
 rm bump.sh-e
-docker build -t kidsloop/krakend:latest --platform=linux/amd64 --build-arg ENV=dev .
-docker tag kidsloop/krakend 246958245973.dkr.ecr.eu-west-2.amazonaws.com/krakend:$VERSION
-docker push 246958245973.dkr.ecr.eu-west-2.amazonaws.com/krakend:$VERSION
+docker build -t kidsloop/krakend-e:latest --platform=linux/amd64 --build-arg ENV=dev .
+docker tag kidsloop/krakend-e:latest $ECR_URL:$VERSION
+docker push $ECR_URL:$VERSION
 sed -i -e "s,image.*$,\image: $ECR_URL:$VERSION," deployment.yaml
 rm deployment.yaml-e
 kubectl apply -f deployment.yaml
