97e9168e1ea0c2f977e05516793f8c88201b9202
Naaman Curtis <ncurtis@kidsloop.live>
2022-04-19 14:13:24 +0100

    feat(krakend): try and get protobuf routing working

---
+++ krakend/protobuf/go.mod
@@ -0,0 +1,18 @@
+module example.com/helloworld
+
+go 1.18
+
+require (
+	github.com/kpacha/krakend-grpc-gateway-post v0.0.0-20190510164258-b43df75c26eb
+	google.golang.org/grpc v1.45.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
---
+++ krakend/protobuf/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/kpacha/krakend-grpc-gateway-post/generated/helloworld"
+	grpc "google.golang.org/grpc"
+)
+
+func main() {
+	port := flag.Int("p", 50051, "port of the service")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+  log.Printf("Hosting gRPC server on :%d", *port)
+	grpcServer := grpc.NewServer()
+	helloworld.RegisterGreeterServer(grpcServer, &server{})
+
+	grpcServer.Serve(lis)
+}
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+	log.Printf("Received: %v", in.Name)
+	return &helloworld.HelloReply{Message: "Hello " + in.Name}, nil
+}
